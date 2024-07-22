bcf542388ec5ddfff59b6f1537dbe720e3600527
Jodeel <joelkamau@stareheboyscentre.ac.ke>
2024-07-22 20:21:38 +0300

    commit02

---
+++ Includes/footer.php
@@ -0,0 +1,28 @@
+<div class="container">
+                <div class="footer-col">
+                    <h1>Support</h1>
+                    <ul type=none>
+                        <li><a href="#"><i class="fas fa-handshake"style='font-size:20px;color:white'></i>Sponsor</li></a>
+                        <li><a href="#"><i class="fas fa-grin-alt"style='font-size:20px;color:white'></i>Help</li></a>
+                        <li><a href="#"><i class="far fa-newspaper"></i>Terms and Conditions</li></a>
+                        <li><a href="#"><i class="fas fa-bell"style='font-size:20px;color:white'></i>Alarts</li></a>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h1>Location</h1>
+                    <ul type=none>
+                        <li><a href="#"><i class="fas fa-map-marker-alt"style='font-size:20px;color:red'></i>Opposite Shauri Mans<br>Near Embenezer Road<br>Nakuru,Naivasha<br>Kenya , Africa</li></a><br>
+                        <li><a href="#"><i class="fas fa-envelope-open"></i>PO Box 13425-100- Embenezer street, Nakuru</li></a>
+                    </ul>
+                </div>
+                <div class="footer-col">
+                    <h1>Contact us</h1>
+                    <ul type=none>
+                        <li><a href="#"><i class="fab fa-facebook"style='font-size:20px;color:white'></i>Facebook</li></a>
+                        <li><a href="#"><i class="fab fa-instagram"style='font-size:20px;color:red'></i>Instagram</li></a>
+                        <li><a href="#"><i class="fab fa-twitter"style='font-size:20px;color:white'></i>Twitter</li></a>
+                        <li><a href="#"><i class="fab fa-whatsapp"style='font-size:20px;color:white'></i>Whatsapp</li></a>
+                        <li><a href="#"><i class="fa fa-envelope"style='font-size:20px;color:red'></i>joelNguru@Gmail.com</li></a>
+                        <li><a href="#"><i class='fas fa-phone' style='font-size:20px;color:white'></i>+254-769-223-883</li></a>
+                    </ul>
+                </div>
