2ad5c3b4976208df55f205b61a7567ad3d51f861
Jodeel <joelkamau@stareheboyscentre.ac.ke>
2024-07-22 19:42:39 +0300

    commit01

---
+++ Includes/navbar.php
@@ -0,0 +1,28 @@
+<div class="navbar">
+<div class="logo">
+            <img src="Resources/css/Pics/infinity.gif" alt="Infinity">
+        </div>
+    <div class="icon">
+       <p> Infinity Cartoons</p><i class="fas fa-grin-squint-tears"style='font-size:40px;color:white'></i>
+    </div>
+    <div class="catchphrase">
+        Where its all about cartoons.<br>
+        Stream,Send,Download,Be Updated..WELCOME
+    </div>
+<div class="menu">
+    <ul type="none">
+        <li><a href="Index.php"style='color:white'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
+  <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
+</svg>Home</li></a>
+        <li><a href="LogIn.php"style='color:white'><i class="fas fa-key"style='font-size:20px;color:white'></i>Log in</li></a>
+        <!-- <li><a href="ContactUs.php"style='color:white'><i class="fas fa-phone-alt"style='font-size:20px;color:white'></i>Contact Us</li> -->
+        <li><a href="About.php"style='color:white'><i class="fas fa-question-circle"style='font-size:20px;color:white'></i>About</li></a>
+        <li><a href="SignUp.php"style='color:white'><i class="fas fa-user-plus"style='font-size:20px;color:white'></i>Sign up</li></a>
+    </ul>
+</div>
+<div class="customers">
+<i class="fa fa-search"style='font-size:15px;color:white'></i>
+    <input type="search" placeholder="What may U be looking for?">
+</div>
+</div>
